901acb42c9fde460952ea2152f3f7298c6d4fc60
iglemp <iglemp@protonmail.com>
2026-07-11 00:16:21 +0200

    Transitioned to SystemVerilog, we're ready for take-off

--- main.v
+++ main.v
@@ -1,25 +1,25 @@
-`define LEDS_NR 6
-
-module top (
-	input clk,
-	input key_i,
-	output [`LEDS_NR-1:0] led
+module main #(
+  parameter integer unsigned LEDS_NR = 6
+) (
+  input clk_i,
+  input key_i,
+  output [LEDS_NR-1:0] led_o
 );
 
-wire key = key_i;
+logic key = key_i;
 
-reg [25:0] ctr_q;
-wire [25:0] ctr_d;
+logic [25:0] ctr_q;
+logic [25:0] ctr_d;
 
 // Sequential code (flip-flop)
 always @(posedge clk) begin
-	if (key) begin
-		ctr_q <= ctr_d;
-	end
+  if (key) begin
+    ctr_q <= ctr_d;
+  end
 end
 
 // Combinational code (boolean logic)
 assign ctr_d = ctr_q + 1'b1;
-assign led = {ctr_q[25:25-(`LEDS_NR - 2)], |ctr_q[25-(`LEDS_NR - 1):25-(`LEDS_NR)]};
+assign led = {ctr_q[25:25-(LEDS_NR - 2)], |ctr_q[25-(LEDS_NR - 1):25-(LEDS_NR)]};
 
 endmodule
--- makefile
+++ makefile
@@ -1,5 +1,5 @@
 build:
-	yosys -p "read_verilog main.v; synth_gowin -json main.json -family gw2a"
+	yosys -p "read_verilog -sv main.v; synth_gowin -json main.json -family gw2a"
 	nextpnr-himbaechel --json main.json --write pnrmain.json --device GW2AR-LV18QN88C8/I7 --vopt family=GW2A-18C --vopt cst=tangnano20k.cst
 	gowin_pack -d GW2A-18C -o pack.fs pnrmain.json
 
